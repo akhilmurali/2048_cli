e80ce75b96a0fa171817e2f2c24dcc515c375666
Akhil Murali <github@akhilmurali.com>
2018-06-23 14:56:54 +0530

    Add boiler plate code to user_interfaces.js

---
+++ user_interface.js
@@ -0,0 +1,35 @@
+const gameLogic = require('./gamelogic');
+var keypress = require('keypress');  //Acquire keypress package
+var gameState = [];
+
+keypress(process.stdin);  //Make process.stdin emit keypress events:
+
+gameState = initializeGameStates(gameState);
+
+
+function printGrid(gameState) {
+    var resultGrid = '';
+    gameState.forEach(array => {
+        array.forEach(element => {
+            resultGrid += "| " + element;
+        });
+        resultGrid += " |"+ "\n"
+    });
+    console.log(resultGrid)
+}
+
+//Process to take Keyboard inputs
+process.stdin.on('keypress', function (ch, key) {
+    console.log(key.name);
+    switch(key.name){
+
+    }
+    if (key && key.ctrl && key.name == 'c')     //This logic is to quit the operations from cmd - press - ctrl+C
+    {
+        process.stdin.pause();
+    }
+
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
